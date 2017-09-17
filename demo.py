d96c125110319992f18ff12681052acd98cd10af
Dimitris Kastaniotis <dkastaniotis@upatras.gr>
2017-09-17 19:07:29 +0300

    updated distance matrix comp.

--- demo.py
+++ demo.py
@@ -1,9 +1,7 @@
 """
   Implementing various dimensionality reduction methods with PyTorch Tensors
 
-  Diffusion Maps, Laplacian EigenMaps, stc
-
-
+  
   Under development. Please use with caution.
 
 """
@@ -15,56 +13,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-# adopted from Fransisco Massa's comment
-# (x - y)^2 = x^2 - 2*x*y + y^2
+ 
+ 
 def distance_matrix(mat):
-    # get the product x * y
-    # here, y = x.t()
-    r = torch.mm(mat, mat.t())
-    # get the diagonal elements
-    diag = r.diag().unsqueeze(0)
-    diag = diag.expand_as(r)
-    # compute the distance matrix
-    D = diag + diag.t() - 2*r
-    return D.sqrt()
-
-# (x - y)^2 = x^2 - 2*x*y + y^2
+    d= ((mat.unsqueeze (0)-mat.unsqueeze (1))**2).sum (2)**0.5
+    return d
+ 
 def similarity_matrix(mat):
-    # get the product x * y
-    # here, y = x.t()
-    r = torch.mm(mat, mat.t())
-    # get the diagonal elements
-    diag = r.diag().unsqueeze(0)
-    diag = diag.expand_as(r)
-    # compute the distance matrix
-    D = torch.exp(-(diag + diag.t() - 2*r) )
+    d= distance_matrix(mat)
+    D = torch.exp(-(d ))
     return D.sqrt()
-
-# (x - y)^2 = x^2 - 2*x*y + y^2
-def diffusion_distance(mat, sigma=8, alpha=0.5):
-    D =distance_matrix(mat);
-    K = torch.exp(-(torch.pow(torch.div(D,sigma) ,2))) # Kernel
-    p = K.sum(1)
-    K1 = K/(torch.pow(p.unsqueeze(1)*p,alpha)) # alpha = 1 Laplace Beltrami, 0.5 Fokker Planck diffusion.
-    v = torch.sqrt(K1.sum(1))
-    A = K1/(v.unsqueeze(1)*v)
-    [u,s,v]=torch.svd(D)
-    u=u/u[:,0].unsqueeze(1)    
-    return K1,u
  
 # Generate Clusters
 mat = torch.cat([torch.randn(500,2)+torch.Tensor([-2,-3]),   torch.randn(500,2)+torch.Tensor([2,1])])
  
 mat = mat[torch.randperm(mat.size(0))]
 plt.scatter(mat[:,0].numpy(),mat[:,1].numpy())
-plt.show()
+plt.show(block=False)
 
 ##-------------------------------------------
 #         Spectral analysis on distance matrix
 ##-------------------------------------------
 d= distance_matrix(mat);
 
-plt.figure(1)
+plt.figure()
 plt.imshow(d.numpy())
 plt.title('Distance Matrix-Before Ordering')
 plt.show(block=False)
@@ -74,7 +46,7 @@ plt.show(block=False)
 
 colors = cm.rainbow(np.linspace(0, 1, mat.size(0)))
 [val, ind] = torch.sort(u[:,1] )
-plt.figure(2)
+plt.figure()
 
 sorted_u  = u[ind,:]
  
@@ -85,15 +57,18 @@ plt.title('Eigenvector-Mapping')
 plt.show(block=False)
 
 
-plt.figure(3)
+plt.figure()
 plt.imshow(d[[ind]][:,ind].numpy())
 plt.show(block=False)
 plt.title('Sorted Matrix');
 
-plt.figure(4)
+plt.figure()
 plt.plot(torch.sort(u[:,1 ])[0].numpy())
-plt.show(block=True)
+plt.show(block=False)
 plt.title("Sorted Eigenvector")
+
+raw_input("Press Enter to exit..") 
+plt.close('all')
 
  
  
