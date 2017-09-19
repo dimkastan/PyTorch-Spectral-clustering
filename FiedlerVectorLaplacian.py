88dd8a9b113beec18bb2cf6378abf028662ce11e
Dimitris Kastaniotis <dkastaniotis@upatras.gr>
2017-09-19 03:04:30 +0300

    minor modifications

--- FiedlerVectorLaplacian.py
+++ FiedlerVectorLaplacian.py
@@ -4,21 +4,22 @@
 % -------------------------------------------------------------
 % grpah partition using the eigenvector corresponding to the second
 % smallest  eigenvalue 
-t=[randn(500,2)-repmat([2,2],500,1) ;randn(500,2)+repmat([2,2],500,1)];
+% grpah partition using the eigenvector corresponding to the second
+% smallest  eigenvalue 
+t=[randn(500,2)+repmat([-2,-2],500,1) ;randn(500,2)+repmat([2,2],500,1)];
 scatter(t(:,1),t(:,2))
-
 W=squareform(pdist(t));
-A=W<3;
+A=W<3;      % create adjacency matrix (set connected notes equal to one)
 D=sum(A,1);
 L= diag(D)-A;
 Lsym = diag(D.^-0.5)*L*diag(D.^-0.5);
 [u,s,v]=svd(Lsym);
-f=find(diag(s)>0.01);
-plot(u(:,f(end)))
-F=u(:,f(end));
+
+figure; plot(u(:, (end-1)))
+F=u(:, (end-1));
 plot(F);title('Second smallest non-zero eigenvalue eigenvector');
-scatter(t(F<0,1),t(F<0,2),'b*');hold on
-scatter(t(F>0,1),t(F>0,2),'g*');
+scatter(t(F<0,1),t(F<0,2),'bo','filled');hold on
+scatter(t(F>0,1),t(F>0,2),'go','filled');
 """
 # Pytorch equivalent code
 ...
